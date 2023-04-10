a6fb39f0d08e43c2dc29f0a7de6f92afc0efc1c0
Aditya <105660945+AdiTheRipper@users.noreply.github.com>
2023-04-10 13:09:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const parallax = document.querySelector('.parallax');
-
-window.addEventListener('scroll', function() {
-	let offset = window.pageYOffset;
-	let speed = parallax.dataset.speed;
-	parallax.style.backgroundPositionY = offset * speed + "px";
-});
-
